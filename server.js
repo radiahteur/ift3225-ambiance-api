2a6fba69960e0fd3e579f0cc4dc83e9ccc236d4b
radiahteur <radiah.mohamed.assoweh@umontreal.ca>
2026-08-14 18:01:52 -0400

    Finalisation backend Phase 3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 async function startServer() {
   try {
     await connectDB();
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
